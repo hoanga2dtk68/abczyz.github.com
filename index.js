edaeae3404320d687df910b06b0f2da41eeee600
hoanga2dtk68 <110059218+hoanga2dtk68@users.noreply.github.com>
2022-09-13 11:34:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 $("#login-button").click(function (event) {
     var userName = document.getElementById("userName").value;
     var pwd = document.getElementById("pwd").value;
-    if (userName == "hoangnguyen" && pwd == "123") {
+    if (userName == "mailan" && pwd == "1309") {
       event.preventDefault();
       $("form").fadeOut(500);
       $(".wrapper").addClass("form-success");
